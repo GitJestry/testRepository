886afeb9ad30b5fc13279a1f9f20ca734985aa25
Julian Meyer <jestry.privat@outlook.de>
2024-02-28 01:02:30 +0100

    pointer test

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <memory>
 
 int main() {
 	printf("Hello World!");
 	printf("fuck git");
+	{
+		int* raw = new int(1);
+		std::unique_ptr<int> ptr = std::make_unique<int>(1);
+	}
+
 }
