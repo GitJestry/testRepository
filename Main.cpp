65c5f61b6cdbd03fb393b78fc9cb68502fe23ed4
Julian Meyer <jestry.privat@outlook.de>
2024-02-28 18:03:02 +0100

    delted filters

--- Main.cpp
+++ Main.cpp
@@ -11,9 +11,6 @@ void method() {
 }
 
 int main() {
-	std::cout << "new line of code in quick test" << std::endl;
-	std::cout << "new line of code in master test" << std::endl;
-
 	printf("Hello World!");
 	printf("fuck git");
 	{
